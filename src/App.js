318dcd634e0926d356f774a79e29fe470fe36268
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-10-25 20:06:45 +0700

    props component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Belajar React BWA</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, quo
-        distinctio quidem nostrum voluptatibus voluptas cupiditate. Quaerat,
-        dolor voluptate placeat ab, sit praesentium, nam minus officia harum
-        corrupti provident voluptatum.
+    <div className="ParentBox">
+      <FotoProduk />
+      <ProdukInfo category="Lebaran" name="Youdora Dora" />
+    </div>
+  );
+}
+
+function FotoProduk() {
+  return (
+    <div class="Foto">
+      <img src="basketball.jpg" alt="" />
+    </div>
+  );
+}
+
+function ProdukInfo(props) {
+  const { category, name } = props;
+  return (
+    <div class="Desc">
+      <p class="cafe">{category}</p>
+      <h1 class="title">{name}</h1>
+      <p class="price">IDR 73.309.999</p>
+      <p class="info">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+        recusandae. Corporis officiis unde hic incidunt. Molestias magni
+        reprehenderit itaque libero, numquam eos odit at sint et consequatur
+        dolorum quia est!
       </p>
     </div>
   );
